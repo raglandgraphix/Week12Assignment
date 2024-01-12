d3766ae113b8f4591d5d06fc8fe91af252780b96
John M. Ragland <john@raglandgraphix.com>
2024-01-12 14:11:35 -0600

    updating before I lose anything

--- race.js
+++ race.js
@@ -1,25 +1,83 @@
 
 
 
+ let URL_ENDPOINT = 'http://localhost:3000/race';
 
-
-
+ let idValue = 0;
 $.get('http://localhost:3000/race').then(data => {
+    idValue = data.length+1;
     $.each(data,function(key,val){
-        // console.log(val.title);
-        $('body').append(`
-        <table id="${val.title}" border = "1">
-        <caption>${val.title}</caption>
-        </table>
+       
+        const tableName = val.title;
+        if(tableName==='100k'){
+            
+           $('#100kTable').append(`
+           <tr>
+           <td>${val.name}</td>
+           <td>${val.age}</td>
+           <td>${val.city}</td>
+           <td>${val.state}</td>
+           <td><button class='remove' id="${val.id}" onclick="removeAthlete(${val.id})">&#128465;</button></td>
+       </tr>
+               
+            
         `);
-        $.each(val,function(key2,val2){
-            $("#"+val.title).append(`<tr>
-                <td>${val2.name}</td>
-                </tr>`
-            );
-        })
+    }else if(tableName==='50k'){
+        $('#50kTable').append(`
+            <tr>
+                <td>${val.name}</td>
+                <td>${val.age}</td>
+                <td>${val.city}</td>
+                <td>${val.state}</td>
+                <td><button class='remove' id="${val.id}" onclick="removeAthlete(${val.id})">&#128465;</button></td>
+            </tr>
+            
+        
+        `);
+    }
+       
+        
+    
     
     });
-   
+    
+    
 
-})
+    });
+
+    $("#submit").on('click',function(e){
+       //e.preventDefault();
+        const distance = document.querySelector('#distance').value;
+        const name = document.querySelector('#fullName').value;
+        const age = document.querySelector('#age').value;
+        const city = document.querySelector('#city').value;
+        const state = document.querySelector('#state').value;
+       console.log(distance);
+        let raceDistance;
+        if(distance==='100k'){
+            raceDistance = '100,000 Kilometers';
+        }else if(distance==='50k'){
+            raceDistance = '50,000 Kilometers';
+        }
+
+       
+       $.post(URL_ENDPOINT,
+            {
+                
+                "distance": raceDistance,
+               "title": distance,
+                "name":name,
+                "age":+age,
+                "city":city,
+                "state":state,
+                "title": distance
+            }
+        );
+
+    });
+
+    const removeAthlete = function(id){
+       $.ajax(`${URL_ENDPOINT}/${id}`,{
+        type: 'DELETE'
+       })
+    };
